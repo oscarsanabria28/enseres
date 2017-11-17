f28a42c337a0a4669aca1280b9e3907eaea7d13a
Oscar Sanabria <oscarsanabria28@gmail.com>
2017-11-17 16:04:15 -0600

    Updating my repo, Developing Billing

--- pages/js/agregarCarrito.js
+++ pages/js/agregarCarrito.js
@@ -23,17 +23,35 @@ function agregarAlCarro(indice)
 				}							
 }
 
-function buscar(	){
+function buscar(){
+
 		var array = JSON.parse("[" + carrito + "]");
+
+		if(array.length==0){
+            document.getElementById("jeje").innerHTML = "No tienes productos";
+		}
+
+    var res=""; total=0;
 		for(var i=0; i<array.length;i++){
 			console.log("carrito: "+array[i])
 			 var prod =array[i];
-			 console.log(prod)
-			 var mueb=muebles[prod];
-			 
-			 
+
+        	res=res+'<div class="row">';
+			res=res+ '<div class="col-sm-3 col-sm-offset-3" align="center">';
+            res=res+muebles[prod][0];
+            res=res+'</div>'
+            res=res+ '<div class="col-sm-3">';
+            res=res+ muebles[prod][1];
+            res=res+ '</div>';
+            res=res+  '</div>';
+            total=total+parseInt(muebles[prod][1]);
+
 			 console.log("name: "+muebles[prod][0]+" apellido: "+muebles[prod][1]);
 		}
+	//alert(res);
+    document.getElementById("productss").innerHTML = res;
+    document.getElementById("totalPaypal").value = total;
+    document.getElementById("total_cart").innerHTML = total;
 		
 }
 
